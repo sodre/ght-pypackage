1ff369b1861ba6e9331f8c7b0b6f7a454753803c
Patrick Sodré <psodre@gmail.com>
2019-12-16 02:41:33 -0500

    Fix issue with flake8 not passing immediately after creating a project.

--- {{cookiecutter.project_repo}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_repo}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -6,7 +6,6 @@ import pytest
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 from click.testing import CliRunner
 {%- endif %}
-from {{cookiecutter.project_namespace}}.{{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
 {%- if cookiecutter.command_line_interface|lower == 'click' and cookiecutter.project_slug != 'cli' %}
 from {{cookiecutter.project_namespace}}.{{ cookiecutter.project_slug }} import cli
 {%- endif %}
--- {{cookiecutter.project_repo}}/{{cookiecutter.project_namespace}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_repo}}/{{cookiecutter.project_namespace}}/{{cookiecutter.project_slug}}/cli.py
@@ -4,8 +4,19 @@ import sys
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 import click
 {%- endif %}
+{%- if cookiecutter.project_slug == 'cli' %}
+from click_plugins import with_plugins
+from entrypoints import get_group_named
 
-{% if cookiecutter.command_line_interface|lower == 'click' %}
+
+@with_plugins(get_group_named("{{cookiecutter.project_namespace}}.cli"))
+@click.group()
+def main(args=None):
+    """{{cookiecutter.project_namespace}} command-line-interface"""
+    return 0
+{%- elif cookiecutter.command_line_interface|lower == 'click' %}
+
+
 @click.command()
 def main(args=None):
     """Console script for {{cookiecutter.project_repo}}."""
