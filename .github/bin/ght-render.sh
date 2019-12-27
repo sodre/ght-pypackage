5157b80e6f296c507bdcd4ac36f6907b8c3d1257
Patrick Sodré <psodre@gmail.com>
2019-12-27 00:34:34 -0500

    Ignore idea

--- .github/bin/ght-render.sh
+++ .github/bin/ght-render.sh
@@ -33,57 +33,72 @@ tree_converged()
     converged $templ $rendered
 }
 
+render_configuration()
+{
+    set -e
+    ght_temp=$(mktemp)
+    declare -i i=1
+    until converged $GHT_CONF $ght_temp; do
+        echo "Rendering $GHT_CONF: Pass $i"
+        stop_rendering_lines=false
+        mv $GHT_CONF $ght_temp
+        while IFS= read line; do
+            in=$(mktemp)
+            printf "%s\n" "$line" > $in
+            if [ "$stop_rendering_lines" == false ]; then
+                out=$(mktemp)
+                render $in $out $ght_temp
+                cat < $out >> $GHT_CONF
+                if ! converged $in $out; then
+                    stop_rendering_lines=true
+                fi
+            else
+              cat < $in >> $GHT_CONF
+            fi
+        done < $ght_temp
+        let i++
+    done
+    #git commit -m "ght: configuration rendered" $GHT_CONF
+}
+
+render_tree_structure()
+{
+    for filter in "-type d" "-type f"; do
+        ght_temp=$(mktemp)
+        until tree_converged $ght_temp ${ght_temp}.rendered $filter ; do
+            paste -- $ght_temp $ght_temp.rendered |
+                while IFS=$'\t' read -r d1 d2; do
+                    if [ $d1 != $d2 ]; then
+                      git mv $d1 $d2 || true
+                    fi
+                done
+        done
+    done
+    #git commit -m 'ght: template structure rendered'
+}
+
+render_tree_content()
+{
+    find . -path ./.git -prune -o -type f  |
+        grep -v './.git' |
+        sort |
+        while IFS=$'\n' read -r in; do
+            out=$(mktemp)
+            echo "rendering ${in}"
+            render $in $out
+            cp -a ${out} ${in}
+            git add ${in}
+        done
+    #git commit -m 'ght: content rendered'
+}
+
 # This is so we can test things locally without screwing up the master branch
 # tmp=$(basename $(mktemp -t ght-XXXX))
 # git checkout -b $tmp
 
-set -e
-ght_temp=$(mktemp)
-declare -i i=1
-until converged $GHT_CONF $ght_temp; do
-    echo "Rendering $GHT_CONF: Pass $i"
-    stop_rendering_lines=false
-    mv $GHT_CONF $ght_temp
-    while IFS= read line; do
-        in=$(mktemp)
-        printf "%s\n" "$line" > $in
-        if [ "$stop_rendering_lines" == false ]; then
-            out=$(mktemp)
-            render $in $out $ght_temp
-            cat < $out >> $GHT_CONF
-            if ! converged $in $out; then
-                stop_rendering_lines=true
-            fi
-        else
-          cat < $in >> $GHT_CONF
-        fi
-    done < $ght_temp
-    let i++
-done
-#git commit -m "ght: configuration rendered" $GHT_CONF
-
-
-for filter in "-type d" "-type f"; do
-    ght_temp=$(mktemp)
-    until tree_converged $ght_temp ${ght_temp}.rendered $filter ; do
-        paste -- $ght_temp $ght_temp.rendered |
-            while IFS=$'\t' read -r d1 d2; do
-                if [ $d1 != $d2 ]; then
-                  git mv $d1 $d2 || true
-                fi
-            done
-    done
-done
-#git commit -m 'ght: template structure rendered'
-
-find . -path ./.git -prune -o -type f  |
-    grep -v './.git' |
-    sort |
-    while IFS=$'\n' read -r in; do
-        out=$(mktemp)
-        echo "rendering ${in}"
-        render $in $out
-        cp -a ${out} ${in}
-        git add ${in}
-    done
-#git commit -m 'ght: content rendered'
+cmd=$1
+if [ ! -z "$cmd" ]; then
+    shift 1
+    $cmd "$@"
+    exit
+fi
