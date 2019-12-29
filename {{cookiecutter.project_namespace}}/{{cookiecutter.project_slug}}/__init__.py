eff848f856a9f2a500127252d94169d4a73e9ff4
Patrick Sodré <psodre@gmail.com>
2019-12-28 22:22:05 -0500

    remove author and email from package, it is duplicative

--- {{cookiecutter.project_namespace}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_namespace}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,2 @@
 """Top-level package for {{ cookiecutter.project_name }}."""
-
-__author__ = """{{ cookiecutter.full_name }}"""
-__email__ = "{{ cookiecutter.email }}"
 from ._version import version as __version__  # noqa: F401
